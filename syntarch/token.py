5b5103d8df417337bdbe252e981daa240859cc60
Chatyusha <hirounicat76@gmail.com>
2023-03-24 09:47:25 +0900

    Update

--- syntarch/token.py
+++ syntarch/token.py
@@ -5,13 +5,18 @@ from .types import TokenTypes
 
 class Token():
     def toJSON(self):
-        return {
+        token_object = {
                 "type" : self.type,
                 "level": self.level,
+                "language": self.language,
                 "contents":self.contents,
                 "children":[i.toJSON() for i in self.children],
                 "items":[i.toJSON() for i in self.items]
-                }
+            }
+        for key in list(token_object):
+            if token_object[key] == None or token_object[key] == []:
+                token_object.pop(key)
+        return token_object
         # if self.children:
         #     return {
         #         "type" : self.type,
@@ -23,8 +28,9 @@ class Token():
         #     else:
         #         return {"type" : self.type, "contents" : self.contents}
     def __init__(self):
-        self.type: str = ""
-        self.level: int = None
+        self.type: str = None
         self.contents : str = None
+        self.level: int = None
+        self.language: str = None
         self.children : list[Token] = []
         self.items : list[Token] = []
